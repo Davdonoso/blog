b04d9c9e615959c3e04ecf8642a2781d1a3bd7d3
David Donoso <waltra2@gmail.com>
2025-02-19 12:21:40 +0100

    app.component.ts
    Importamos nuestra interface IBlog con 2 noticias de muestra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FormularioComponent } from "./components/formulario/formulario.component";
+import { ListanoticiasComponent } from "./components/listanoticias/listanoticias.component";
+import { IBlog } from './interfaces/iblog.interface';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ FormularioComponent, ListanoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog';
+ // Array de Noticias muestra
+ 
+ arrNoticias: IBlog[]= [
+  {titulo:"Yamaha XMax 125", imagen:"https://blog.mundimoto.com/wp-content/uploads/2023/08/Numero__Nombre-del-articulo__Imagen_Blog-980x613.jpg",textoNoticia:"¿Estás buscando una moto con estilo, potencia y maniobrabilidad? La Yamaha TMAX es una excelente opción. Esta moto deportiva y elegante es una de las más populares en su categoría, y con razón. Si estás buscando una moto que te permita disfrutar del paseo y llegar al destino con estilo, sigue leyendo. En este artículo, te presentaremos todas las características y detalles de la Yamaha TMAX.",fechaPublicacion:'2023-01-28'},
+  {titulo:"Honda SH 300", imagen:"https://blog.mundimoto.com/wp-content/uploads/2023/07/Numero__Nombre-del-articulo__Imagen_Blog-980x613.jpg",textoNoticia:"Si eres un amante de las dos ruedas y te gusta sentir la velocidad entonces la moto Honda Sh 300 es una opción que no puedes pasar por alto. En esta ocasión, te presentaremos todo lo que necesitas saber sobre este modelo para que puedas decidir si es la moto perfecta para ti.", fechaPublicacion:'2023-02-28'},
+ ]
 }
