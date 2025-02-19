c3c7d32ea57560497fe36289600e576d46255d48
David Donoso <waltra2@gmail.com>
2025-02-19 12:31:22 +0100

    Formulario.ts
    definimos el tipo de formulario y sus validaciones

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-formulario',
   imports: [],
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+// Se define el formulario del blog
+formNoticias: FormGroup;
 
+
+constructor() {
+  // Inicializamos el formulario con los campos requeridos
+  this.formNoticias = new FormGroup({
+    tituloNoticia: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    imagenNoticia: new FormControl('', [Validators.required, Validators.pattern('^https?:\/\/.*\.(jpg|jpeg|png|webp)$')]), 
+    textoNoticia: new FormControl('', [Validators.required, Validators.minLength(15)]),
+    fechaNoticia: new FormControl('', [Validators.required])
+  }); 
+}
 }
