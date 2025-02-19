24ffed6339053e474c08631a42d0ed6087778cf0
David Donoso <waltra2@gmail.com>
2025-02-19 12:12:10 +0100

    Lista noticias
    Creacion del componente listanoticias (html,css y ts)

---
+++ src/app/components/listanoticias/listanoticias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listanoticias',
+  imports: [],
+  templateUrl: './listanoticias.component.html',
+  styleUrl: './listanoticias.component.css'
+})
+export class ListanoticiasComponent {
+
+}
